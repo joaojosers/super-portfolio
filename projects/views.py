1be660dec42d61881e8b4f5ea4c4d216ff54ed3a
João José <joaojose@Joaos-MacBook-Pro.local>
2024-04-10 09:27:04 -0300

    debugging pyproject-toml

--- projects/views.py
+++ projects/views.py
@@ -32,3 +32,19 @@ class ProfileViewSet(viewsets.ModelViewSet):
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+    
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            profile_id = kwargs.get("pk")
+            profile = Profile.objects.get(id=profile_id)
+            return render(
+                request,
+                "profile_detail.html",
+                {"profile": profile}
+            )
+        return super().retrieve(request, *args, **kwargs)
