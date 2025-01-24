b78f4f9566471bf379da065efada5c072dcb4ee6
João José <joaojose@Joaos-MacBook-Pro.local>
2025-01-24 15:53:53 -0300

    commenting file

--- projects/apps.py
+++ projects/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ProjectsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "projects"
+# This is the apps.py file for the projects app.
